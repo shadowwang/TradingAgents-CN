5ccd9e9214edca144044a94b98fefad27e85c642
parsonswang <parsonswang@tencent.com>
2025-07-27 15:09:26 +0800

    feat:新增获取股票信息接口

--- app/main.py
+++ app/main.py
@@ -1,12 +1,15 @@
 import uvicorn
 from fastapi import FastAPI
 
+from tradingagents.utils.stock_validator import get_stock_preparer
 # 创建API应用
 app = FastAPI()
 
-@app.get("/")
-async def root():
-  return {"message": "Hello World"}
+@app.get("/v1/get_stock_data/{stock_code}")
+async def get_stock_data(stock_code: str):
+  preparer = get_stock_preparer()
+  result = preparer.prepare_stock_data(stock_code)
+  return result
 
 if __name__ == '__main__':
   uvicorn.run(app, host='0.0.0.0', port=6006, log_level="info", workers=1)
