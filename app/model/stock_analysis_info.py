1ba4fe2b69d7d8a8e4e458196c0b3b4bfd9ebd53
parsonswang <parsonswang@tencent.com>
2025-08-18 17:26:06 +0800

    feat: 部分参数声明可为空

--- app/model/stock_analysis_info.py
+++ app/model/stock_analysis_info.py
@@ -1,9 +1,11 @@
+from typing import Optional
+
 from pydantic import BaseModel
 
 class StockAnalysisInfo(BaseModel):
     stock_code: str
     stock_name: str
-    analysis_date: str
-    analysts: str
-    research_depth: int
+    analysis_date: Optional[str] = None
+    analysts: Optional[str] = None
+    research_depth: Optional[int] = None
     id: str
